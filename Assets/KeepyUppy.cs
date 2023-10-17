74a90ce06739584a38a7ebef9404f5643b41e629
E-LAMB <aatkinson2022@elam.co.uk>
2023-10-17 12:44:01 +0100

    Beginning Keepy Uppy

--- Assets/KeepyUppy.cs
+++ Assets/KeepyUppy.cs
@@ -16,24 +16,51 @@ public class KeepyUppy : MonoBehaviour
 
     public float speed = 20f;
 
+    public bool currently_playing;
+
+    public float health_disc_a;
+    public float health_disc_b;
+
+    public float button_cooldown;
+
+    public float difficulty = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void GameSetup()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
+
+        if (currently_playing)
+        {
+
+            height_disc_a
+
+
+
+        }
+
         if (currently_open)
         {
-            door_a_rotation -= Time.deltaTime * speed; if (door_a_rotation < 0f) { door_a_rotation = 0f; }
+            door_a_rotation -= Time.deltaTime * speed; if (door_a_rotation < -90f) { door_a_rotation = -90f; }
+            door_b_rotation += Time.deltaTime * speed; if (door_b_rotation > 88f) { door_b_rotation = 88f; }
 
         } else
         {
-            door_a_rotation += Time.deltaTime * speed; if (door_a_rotation > 90f) { door_a_rotation = 90f; }
+            door_a_rotation += Time.deltaTime * speed; if (door_a_rotation > 0f) { door_a_rotation = 0f; }
+            door_b_rotation -= Time.deltaTime * speed; if (door_b_rotation < -0f) { door_b_rotation = -0f; }
         }
 
-        door_a.transform.eulerAngles = new Vector3(-0f, door_a_rotation, -90f);
+        door_a.transform.eulerAngles = new Vector3(-180f, door_a_rotation, -90f);
+        door_b.transform.eulerAngles = new Vector3(-180f, door_b_rotation, -90f);
     }
 }
